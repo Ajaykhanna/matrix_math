85dc42bcf49e57a0d4457e82e23f05979ffcef0b
wcdawn <wdawn730@gmail.com>
2016-06-24 08:19:45 -0400

    Working matrix solver

--- matrix_solve/matrix_solve.f95
+++ matrix_solve/matrix_solve.f95
@@ -1,159 +1,74 @@
 program matrixsolve
+use matrixtools
 IMPLICIT NONE
 
-real(8),allocatable, dimension(:,:) :: identity, mat, invmat, ansmat, errormat
-real(8)                             :: increment, error, slope, initial
-integer                             :: ios, row, col, r, c, rowans, colans, i, j
-character(80)                       :: fname
+real(8),dimension(:,:),allocatable :: coeff, ans, lower, upper
+real(8),dimension(:),  allocatable :: sol, y
+real(8) :: lower_sum, upper_sum
+integer :: ios, row_coeff, col_coeff, row_ans, col_ans, i,j
+character(80) :: fname_coeff, fname_ans
 
-interface
-	subroutine matrixwrite(row,col,mat)
-		integer, intent(in)  :: row, col
-		integer              :: r, c
-		real(8), intent(in)  :: mat(:,:)
-	endsubroutine matrixwrite
-	
-	subroutine matrixmul(row1,col1,mat1,row2,col2,mat2,ansmat,rowans,colans)
-		integer, intent(in)  :: row1, col1, row2, col2
-		integer              :: dis1, dis2, sim, i, j, k
-		integer, intent(out) :: rowans, colans
-		real(8), intent(in),  dimension(:,:)              :: mat1, mat2
-		real(8), intent(out), dimension(:,:), allocatable :: ansmat
-	endsubroutine matrixmul
-endinterface
+101 format(a) ! plain text descriptor
+102 format(e12.6) 
 
-101 format(a)     ! plain text descriptor
-102 format(e15.6) ! exponential, length = 15, decimal = 6
-
-
-write(*,101) 'matrix file must have (row,col) in 1st line'
-write(*,101) 'enter filename of matrix'
-read(*,*) fname
-
-open(unit = 11, file = fname, status = 'old', action = 'read', iostat = ios);
-	if (ios .ne. 0) stop('error opening unit 11')
-
-read(11,*) row, col
-if (row .ne. col) stop('right now this only works for square matricies')
-allocate(mat(row,col),identity(row,col),invmat(row,col))
-allocate(ansmat(row,col),errormat(row,col))
-do r = 1,row
-	read(11,*) mat(r,:)
-enddo
-
-! build identity matrix
-do r = 1,row
-	do c = 1,col
-		if (r .eq. c) then
-			identity(r,c) = 1.0d0
-		else
-			identity(r,c) = 0.0d0
-		endif
-	enddo
-enddo
-
-! set solution to all 1's
-initial = 1.0d0
-invmat(:,:) = initial
-
-! calculate and record the total error
-call matrixmul(row,col,mat,row,col,invmat,ansmat,rowans,colans)
-error = 0.0d0
-do r = 1,row
-	do c = 1,col
-		error = error + (ansmat(r,c) - identity(r,c))
-	enddo
-enddo
-! debug write
-! write(*,102) error
-! call matrixwrite(rowans,colans,ansmat)
-
-! set "bump" value
-increment = 0.5d0
-! bump each and record the total error
-! assume the error is linear
-do r = 1,row
-	do c= 1,col
-		invmat(r,c) = invmat(r,c) + increment
-		call matrixmul(row,col,mat,row,col,invmat,ansmat,rowans,colans)
-		! calculate the new error based on this "bump"
-		errormat(r,c) = 0.0d0
-		do i = 1,row
-			do j = 1,col
-				errormat(r,c) = errormat(r,c) + (ansmat(r,c) - identity(r,c))
-			enddo
-		enddo
-		! set the error to 0 (linearly)
-		slope = (errormat(r,c) - error) / (increment)
-		invmat(r,c) = initial - (error / slope)		
-	enddo
-enddo
-
-
-		
-
-
-call matrixwrite(row,col,invmat)
+! take input of matrix file name
+write(*,101) 'matrix file must have (row,col) in first line'
+write(*,101) 'enter filename of coefficient matrix'
+read(*,*) fname_coeff
+write(*,101) 'enter filename of answer matrix'
+read(*,*) fname_ans
 write(*,*)
-call matrixmul(row,col,mat,row,col,invmat,ansmat,rowans,colans)
-call matrixwrite(rowans,colans,ansmat)
+
+! open matrix file
+open(unit = 11, file = fname_coeff, status = 'old', action = 'read', iostat = ios)
+if (ios .ne. 0) stop('error opening unit 11')
+open(unit = 12, file = fname_ans, status = 'old', action = 'read', iostat = ios)
+if (ios .ne. 0) stop('error opening unit 12')
+
+! read matrix file
+read(11,*) row_coeff, col_coeff
+allocate(coeff(row_coeff,col_coeff))
+read(12,*) row_ans, col_ans
+allocate(ans(row_ans,col_ans))
+allocate(sol(row_ans),y(row_ans))
+
+if ((col_coeff .ne. row_ans) .or. (col_ans .ne. 1)) stop('not sure that is a linear system...')
+
+do i = 1,row_coeff
+	read(11,*) coeff(i,:)
+enddo
+do i = 1,row_ans
+	read(12,*) ans(i,:)
+enddo
+
+write(*,101) 'echo coefficient input'
+call matrixwrite(row_coeff,col_coeff,coeff)
+write(*,101) 'echo answer input'
+call matrixwrite(row_ans,col_ans,ans)
+
+do i = 1,row_coeff
+	lower_sum = 0.0d0
+	do j = 1,(i - 1)
+		lower_sum = lower_sum + lower(i,j) * y(j)
+	enddo
+	y(i) = (ans(i,1) - lower_sum) !/ lower(i,i)
+enddo
+
+do i = row_coeff,1,-1
+	upper_sum = 0.0d0
+	do j = i, (row_coeff - 1)
+		upper_sum = upper_sum + upper(i,j) * sol(j)
+	enddo
+	sol(i) = (y(i) - upper_sum) / upper(i,i)
+enddo
+
+do i = 1,row_ans
+	write(*,102) y(i)
+enddo
+write(*,*)
+do i = 1,row_ans
+	write(*,102) sol(i)
+enddo
+write(*,*)
 
 endprogram matrixsolve
-
-subroutine matrixwrite(row,col,mat)
-	IMPLICIT NONE
-	integer, intent(in)  :: row, col
-	integer              :: r, c
-	real(8), intent(in)  :: mat(:,:)
-	
-	101 format(a)     ! plain text descriptor
-	102 format(e15.6) ! exponential, length = 15, decimal = 6
-	
-	do r = 1, row
-		write(*,101,advance= 'no') '[ '
-		do c = 1, col
-			write(*,102,advance = 'no') mat(r,c)
-		enddo
-		write(*,101) ' ]'
-	enddo
-endsubroutine matrixwrite
-
-subroutine matrixmul(row1,col1,mat1,row2,col2,mat2,ansmat,rowans,colans)
-	IMPLICIT NONE
-	integer, intent(in)  :: row1, col1, row2, col2
-	integer              :: dis1, dis2, sim, i, j, k
-	integer, intent(out) :: rowans, colans
-	real(8), intent(in),  dimension(:,:)              :: mat1, mat2
-	real(8), intent(out), dimension(:,:), allocatable :: ansmat
-	
-	101 format(a) ! plain text descriptor
-	
-	! check if matrix multiplication is allowed
-	if (col1 .eq. row2) then
-		sim = col1
-		dis1 = row1
-		dis2 = col2
-	elseif (row1 .eq. col2) then
-		write(*,101) 'I think you want mat2 * mat1'
-		write(*,101) 'I cant do that right now'
-		write(*,101) 'Switch mat1 & mat2 in input.'
-		stop 'END OF PROGRAM'
-	else
-		stop('matrix multiplication not possible based on dimensions')
-	endif
-	
-	allocate(ansmat(dis1,dis2))
-	rowans = dis1
-	colans = dis2
-	
-	do i = 1, dis1
-		do j = 1, dis2
-			ansmat(i,j) = 0.0d0
-			do k = 1, sim
-				! debug write
-				! write(*,'(3e15.6,3i3)') mat1(i,k), mat2(k,i), (mat1(i,k) * mat2(k,i)), i, j, k
-				ansmat(i,j) = ansmat(i,j) + (mat1(i,k) * mat2(k,j))
-			enddo
-		enddo
-	enddo
-endsubroutine
---
+++ matrix_solve/matrix_tools.f95
@@ -0,0 +1,142 @@
+module matrixtools
+IMPLICIT NONE
+
+contains
+subroutine matrixwrite(row,col,mat)
+	IMPLICIT NONE
+	integer, intent(in)  :: row, col
+	integer              :: r, c
+	real(8), intent(in)  :: mat(:,:)
+	
+	101 format(a)     ! plain text descriptor
+	102 format(e12.6,x) ! exponential, length = 12, decimal = 6, follow w/ space
+	
+	do r = 1, row
+		write(*,101,advance= 'no') '[ '
+		do c = 1, col
+			write(*,102,advance = 'no') mat(r,c)
+		enddo
+		write(*,101) ']'
+	enddo
+	write(*,101) ! blank line
+	
+endsubroutine matrixwrite
+
+subroutine matrixmul(row1,col1,mat1,row2,col2,mat2,rowans,colans,ansmat)
+	IMPLICIT NONE
+	integer, intent(in)  :: row1, col1, row2, col2
+	integer              :: dis1, dis2, sim, i, j, k
+	integer, intent(out) :: rowans, colans
+	real(8), intent(in),  dimension(:,:)              :: mat1, mat2
+	real(8), intent(out), dimension(:,:), allocatable :: ansmat
+	
+	101 format(a) ! plain text descriptor
+	
+	! check if matrix multiplication is allowed
+	if (col1 .eq. row2) then
+		sim = col1
+		dis1 = row1
+		dis2 = col2
+	elseif (row1 .eq. col2) then
+		write(*,101) 'I think you want mat2 * mat1'
+		write(*,101) 'I cant do that right now'
+		write(*,101) 'Switch mat1 & mat2 in input.'
+		stop 'END OF PROGRAM'
+	else
+		stop('matrix multiplication not possible based on dimensions')
+	endif
+	
+	allocate(ansmat(dis1,dis2))
+	rowans = dis1
+	colans = dis2
+	
+	do i = 1, dis1
+		do j = 1, dis2
+			ansmat(i,j) = 0.0d0
+			do k = 1, sim
+				! debug write
+				! write(*,'(3e15.6,3i3)') mat1(i,k), mat2(k,i), (mat1(i,k) * mat2(k,i)), i, j, k
+				ansmat(i,j) = ansmat(i,j) + (mat1(i,k) * mat2(k,j))
+			enddo
+		enddo
+	enddo
+endsubroutine matrixmul
+
+subroutine matrixdecompose(row,col,mat,lower,upper)
+	IMPLICIT NONE
+	integer,intent(in) :: row, col
+	integer :: ansrow, anscol
+	integer :: i, j, k, w
+	real(8),dimension(:,:),intent(in) :: mat
+	real(8),dimension(:,:),intent(out),allocatable :: upper, lower
+	real(8),dimension(:,:),allocatable :: ansmat
+	real(8) :: asum, bsum
+	
+	101 format(a) ! plain text descriptor
+	
+	! Doolittle Algorithm
+	! See wiki PDF
+	
+	if (row .ne. col) stop('LU decomposition is only defined for squqre matricies')
+	allocate(lower(row,col),upper(row,col))
+	lower(:,:) = 0.0d0
+	upper(:,:) = 0.0d0
+	do i = 1,row
+		lower(i,i) = 1.0d0
+	enddo
+	
+	do i = 1,row
+		do j = i,row
+			asum = 0.0d0
+			do w = 1,(i - 1)
+				asum = asum + lower(i,w) * upper(w,j)
+			enddo
+			upper(i,j) = mat(i,j) - asum
+		enddo
+		do k = 1,col
+			bsum = 0.0d0
+			do w = 1,(i - 1)
+				bsum = bsum + lower(k,w) * upper(w,i)
+			enddo
+			lower(k,i) = (mat(k,i) - bsum) / upper(i,i)
+		enddo
+	enddo
+	
+	! Multiply L * U to make sure it worked
+	! TO-DO: Remove this later
+	! call matrixmul(row,col,lower,row,col,upper,ansrow,anscol,ansmat)
+	! call matrixwrite(ansrow,anscol,ansmat)
+	! do i = 1,col
+		! do j = 1,row
+			! if (ansmat(i,j) .ne. mat(i,j)) then
+				! write(*,'(2i3)') i,j
+				! stop('didnt work')
+			! endif
+		! enddo
+	! enddo
+	! write(*,101) 'decomposition successful'
+	
+endsubroutine matrixdecompose
+
+subroutine matrixdet(row,col,mat,det_mat)
+	IMPLICIT NONE
+	integer,intent(in) :: row, col
+	integer :: i
+	real(8),dimension(:,:),intent(in) :: mat
+	real(8),intent(out) :: det_mat
+	real(8),dimension(:,:),allocatable :: lower, upper
+	
+	if (row .ne. col) stop('determinant only defined for square matricies')
+	
+	! First, decompose the matrix
+	call matrixdecompose(row,col,mat,lower,upper)
+	! determinant of a triangular matrix is the multiplication of the "angle"
+	! det_lower = 1.0d0 ! definition from Doolittle algorithm
+	det_mat = 1.0d0
+	do i = 1,row
+		det_mat = det_mat * upper(i,i)
+	enddo
+
+endsubroutine matrixdet
+	
+endmodule matrixtools
